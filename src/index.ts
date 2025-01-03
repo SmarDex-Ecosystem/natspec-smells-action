0979f2ec7f8bcb41fc1f2ca302a24e9dfaed119c
samooyo <samidarnaud@gmail.com>
2025-01-03 15:57:16 +0100

    fix(standard): fix all errors

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,5 @@ import { runPost } from './post'
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 run()
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 runPost()
